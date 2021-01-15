40add61e840de85df8d129bc4c42efff82653206
altheos <ardef@free.fr>
2021-01-15 17:56:28 +0100

    Fix serial mouse not working

--- src/pc.c
+++ src/pc.c
@@ -945,7 +945,6 @@ pc_reset_hard_init(void)
     }
     fdd_reset();
 
- 	mouse_reset();
     /* Reset the Hard Disk module. */
 //FIXME: move to disk_reset()
     hdc_reset();
@@ -1107,9 +1106,7 @@ pc_thread(void *param)
 
 		plat_blitter(0);
 
-#ifdef USE_DINPUT
 		mouse_poll();
-#endif
 
 		joystick_process();
 
