c526afa4380a48ad044228830944c51f177ace1a
waltje <decwiz@yahoo.com>
2023-02-23 20:06:14 -0500

    OOps

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -71,9 +71,9 @@
 
     echo ; echo "Downloading build dependencies.."
     curl -# "${EXTDEP_URL}" | tar xzf -
-    if [ $? != 0 ]; then
-	exit 1
-    fi
+#   if [ $? != 0 ]; then
+#exit 1
+#   fi
 
     # Build the project.
     echo ; echo "Building #${BUILD} target ${TARGET}"
